4b76ff611baeebfcc1d19541a90dff9b0560988a
Grisuno <grisuno@gmail.com>
2024-06-25 23:31:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def index():
     return render_template('index.html', form=form, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
